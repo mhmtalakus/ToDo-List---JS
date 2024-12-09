c05534b9c73eb945b2b06845eafa953ecf0a342c
mhmtalakus <mehmetalakus02@icloud.com>
2024-12-09 17:04:21 +0300

    The elements were assigned to variables in the script section.

---
+++ script.js
@@ -0,0 +1,14 @@
+const containerElement = document.querySelector("#main.container");
+
+const inputElement = document.querySelector("#textValue");
+
+// We select the button elements and assign them to variables.
+
+const addButton = document.getElementById("add");
+const updateButton = document.getElementById("update");
+const deleteButton = document.getElementById("delete");
+const clearButton = document.getElementById("clear");
+
+// We select the ul element and assign it to a variable.
+
+const ulElement = document.querySelector(".card-body>ul.list-group");
